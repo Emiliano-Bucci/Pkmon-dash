18400a8841728354772b1e495c6ee20d23cd6ca5
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-02-09 09:10:03 +0100

    feat: add global styles and fonts

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,9 +1,12 @@
+import { Global } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { globalStyles } from "./theme/globalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
